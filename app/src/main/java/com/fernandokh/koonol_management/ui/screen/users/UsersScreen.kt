85d89532fc25abdf12e8ad361be2778edd6106af
FernandoKumul <ferdesarrollador11@outlook.com>
2024-10-12 19:29:40 -0500

    DropDownMenu reusable

--- app/src/main/java/com/fernandokh/koonol_management/ui/screen/users/UsersScreen.kt
+++ app/src/main/java/com/fernandokh/koonol_management/ui/screen/users/UsersScreen.kt
@@ -1,26 +1,60 @@
 package com.fernandokh.koonol_management.ui.screen.users
 
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.fernandokh.koonol_management.R
 import com.fernandokh.koonol_management.Screen
 import com.fernandokh.koonol_management.ui.components.router.TopBarMenuTitle
 import com.fernandokh.koonol_management.ui.theme.KoonolmanagementTheme
+import com.fernandokh.koonol_management.utils.MenuItem.Option
+import com.fernandokh.koonol_management.utils.MenuItem.Divider
 
 @Composable
 fun UsersScreen(navController: NavHostController, drawerState: DrawerState) {
@@ -37,19 +71,139 @@ fun UsersScreen(navController: NavHostController, drawerState: DrawerState) {
             }
         },
         content = { innerPadding ->
-            Column (modifier = Modifier.padding(innerPadding)) {
-                Text("Aqui va el contenido")
+            LazyColumn(modifier = Modifier.padding(innerPadding)) {
+                item { CardUserItem(navController) }
             }
         },
     )
 }
 
-@Preview
+@Composable
+fun CardUserItem(navController: NavHostController) {
+    var menuOpen by remember { mutableStateOf(false) }
+    Row(
+        Modifier.padding(16.dp, 20.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.default_user),
+            contentDescription = "img_user",
+            modifier = Modifier
+                .size(48.dp)
+                .clip(CircleShape)
+        )
+        Column(modifier = Modifier.weight(1f)) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    fontWeight = FontWeight.Medium,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    text = "Jonh Doe Default",
+                    fontSize = 17.sp
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Box(
+                    modifier = Modifier
+                        .size(6.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.onPrimaryContainer)
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    text = "Rol",
+                    fontSize = 16.sp
+                )
+            }
+            Text(
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                text = "defaultemail@gmail.com",
+                fontSize = 14.sp
+            )
+        }
+        IconButton(
+            modifier = Modifier
+                .size(32.dp)
+                .align(Alignment.CenterVertically),
+            onClick = { menuOpen = true }
+        ) {
+            Icon(imageVector = Icons.Filled.MoreVert, contentDescription = "options")
+            UserMenu(
+                expanded = menuOpen,
+                onDismiss = { menuOpen = false },
+                onItemClick = { option ->
+                    when (option.name) {
+                        "Más información" -> navController.navigate(Screen.CreateUser.route)
+                        "Editar" -> navController.navigate(Screen.EditUser.route)
+                        "Borrar" -> {/**/}
+                    }
+                })
+        }
+    }
+    HorizontalDivider(thickness = 1.dp)
+}
+
+@Composable
+fun UserMenu(expanded: Boolean, onDismiss: () -> Unit, onItemClick: (Option) -> Unit) {
+    val options = listOf(
+        Option(
+            "Más información",
+            ImageVector.vectorResource(R.drawable.ic_user_line),
+            MaterialTheme.colorScheme.onBackground
+        ),
+        Divider,
+        Option("Editar", Icons.Filled.Home, MaterialTheme.colorScheme.primary),
+        Divider,
+        Option(
+            "Borrar",
+            ImageVector.vectorResource(R.drawable.ic_logout_circle),
+            MaterialTheme.colorScheme.error
+        ),
+        Divider,
+    )
+
+    DropdownMenu(
+        expanded = expanded,
+//        modifier = Modifier.shadow(),
+        onDismissRequest = { onDismiss() }
+//                Modifier.background(MaterialTheme.colorScheme.background)
+    ) {
+
+        options.forEach { option ->
+            when (option) {
+                is Option -> {
+                    DropdownMenuItem(
+                        onClick = {
+                            onDismiss()
+                            onItemClick(option)
+                        },
+                        text = {
+                            Column {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Icon(
+                                        tint = option.color,
+                                        imageVector = option.icon,
+                                        contentDescription = option.name
+                                    )
+                                    Spacer(modifier = Modifier.width(6.dp))
+                                    Text(text = option.name, color = option.color)
+                                }
+                            }
+                        }
+                    )
+                }
+
+                Divider -> HorizontalDivider()
+            }
+        }
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
 @Composable
 fun PrevUsersScreen() {
     val navController = rememberNavController()
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    KoonolmanagementTheme (dynamicColor = false) {
+    KoonolmanagementTheme(dynamicColor = false) {
         UsersScreen(navController, drawerState)
     }
 }
---
+++ app/src/main/java/com/fernandokh/koonol_management/utils/MenuItem.kt
@@ -0,0 +1,16 @@
+package com.fernandokh.koonol_management.utils
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed interface MenuItem {
+
+    data class Option(
+        val name: String,
+        val icon: ImageVector,
+        val color: Color,
+        val enabled: Boolean = true
+    ) : MenuItem
+
+    object Divider : MenuItem
+}
