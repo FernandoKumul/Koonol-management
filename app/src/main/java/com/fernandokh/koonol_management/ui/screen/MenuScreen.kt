12c1c8b58561e82f0eec56031a25e7d476c0f2f3
FernandoKumul <ferdesarrollador11@outlook.com>
2024-10-27 15:51:46 -0500

    Menu main

--- app/src/main/java/com/fernandokh/koonol_management/ui/screen/MenuScreen.kt
+++ app/src/main/java/com/fernandokh/koonol_management/ui/screen/MenuScreen.kt
@@ -1,21 +1,166 @@
 package com.fernandokh.koonol_management.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.fernandokh.koonol_management.Screen
+import androidx.navigation.compose.rememberNavController
+import com.fernandokh.koonol_management.ui.components.router.RouteItem
+import com.fernandokh.koonol_management.ui.theme.KoonolmanagementTheme
+import com.fernandokh.koonol_management.utils.routes
 
 @Composable
 fun MenuScreen(navController: NavHostController) {
-    Column {
-        Text("Menú")
-        Button(onClick = { navController.navigate(Screen.Login.route) }) {
-            Text("Cerrar sesión")
+    Scaffold { innerPadding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.primaryContainer)
+        ) {
+            val text = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.primary)) {
+                    append("¿")
+                }
+                withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.onBackground)) {
+                    append("Qué deseas ")
+                }
+                withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.primary)) {
+                    append("gestionar?")
+                }
+            }
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)
+                    .height(148.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Bienvenido",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Medium,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Text(
+                    text = text,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 24.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            ListBtn(navController)
         }
-        Button(onClick = { navController.navigate(Screen.Users.route) }) {
-            Text("Usuarios")
+    }
+}
+
+@Composable
+private fun ListBtn(navController: NavHostController) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier
+            .fillMaxSize()
+            .clip(RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp))
+            .background(MaterialTheme.colorScheme.background),
+        contentPadding = PaddingValues(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        val isOddCount = routes.size % 2 == 1
+        val itemCount = if (isOddCount) routes.size - 1 else routes.size
+
+        items(itemCount) { index ->
+            RouteBtn(
+                onClick = { navController.navigate(routes[index].routeName) },
+                route = routes[index]
+            )
         }
+        if (isOddCount) {
+            item(span = { GridItemSpan(2) }) {
+                val lastRoute = routes.last()
+                RouteBtn(
+                    onClick = { navController.navigate(lastRoute.routeName) },
+                    route = lastRoute,
+                    modifier = Modifier.fillMaxWidth(0.5f)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun RouteBtn(route: RouteItem, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Box(
+        contentAlignment = Alignment.Center,
+    ) {
+        OutlinedButton(
+            onClick = { onClick() },
+            modifier = modifier
+                .fillMaxWidth()
+                .height(148.dp),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Column(
+                Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Icon(
+                    painter = painterResource(route.icon),
+                    contentDescription = route.routeName,
+                    modifier = Modifier.size(48.dp),
+                    tint = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+                Spacer(Modifier.height(4.dp))
+                Text(
+                    text = route.text,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = 16.sp
+                )
+            }
+        }
+    }
+
+}
+
+
+@Preview(showSystemUi = false, showBackground = true)
+@Composable
+fun PrevMenuScreen() {
+    val navController = rememberNavController()
+    KoonolmanagementTheme(dynamicColor = false) {
+        MenuScreen(navController)
     }
 }
